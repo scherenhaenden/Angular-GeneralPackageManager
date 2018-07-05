9d2f328672103b0dccee3fec924cec0b1c488b22
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-05 14:54:02 +0200

    aaprouting added to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
