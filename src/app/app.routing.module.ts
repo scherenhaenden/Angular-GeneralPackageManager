bd28b5cc409bb2909e5f1997815af9364fe643d6
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-05 13:56:06 +0200

    turn routing into routing class module

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,16 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { APP_ROOT } from '@angular/core/src/di/scope';
+
+const routes: Routes = [
+  {  },
+];
+
+//export const App.routingRoutes = RouterModule.forChild(routes);
+
+@NgModule({
+  imports:RouterModule.forRoot(app_roots {})
+
+})
+
+export class AppRoutingModule {}
