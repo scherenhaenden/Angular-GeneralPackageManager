8d327dc39c25e28fbc861c99e7dd6e0304a91565
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-05 13:57:48 +0200

    created first route. Home Route

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 import { APP_ROOT } from '@angular/core/src/di/scope';
 
 const routes: Routes = [
-  {  },
+  {path: '', pathMatch: 'full',redirectTo: '/home'  },
 ];
 
 //export const App.routingRoutes = RouterModule.forChild(routes);
 
 @NgModule({
-  imports:RouterModule.forRoot(app_roots {})
+  imports:RouterModule.forRoot(routes {})
 
 })
 
