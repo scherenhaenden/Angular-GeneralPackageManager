43ac99a1a6f2d31a30238f9b245eaf02e76cfc1f
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-05 13:33:33 +0200

    warning for loading only once created

--- src/app/tools/guards/EnsureModuleLoadedOnceGuard.ts
+++ src/app/tools/guards/EnsureModuleLoadedOnceGuard.ts
@@ -1,5 +1,7 @@
 export class EnsureModuleLoadedOnceGuard {
-    constructor(targetModuke: any){
-        
+    constructor(targetModule: any){
+        if(targetModule) {
+            throw new Error(`${targetModule.constructor.name} has been loaded alread. Try Adding this Module only once in the AppModule.`);
+        }
     }
 }
