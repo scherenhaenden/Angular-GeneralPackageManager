ad830647759f73220ed26feaf1beeb16b1510bac
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-05 14:06:44 +0200

    home and router outlet added
    home was adding on the routing
    router outlet was added on the app component html

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,16 +1,16 @@
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { APP_ROOT } from '@angular/core/src/di/scope';
+
 
 const routes: Routes = [
   {path: '', pathMatch: 'full',redirectTo: '/home'  },
+  {path: '/home', pathMatch: 'full',redirectTo: '/home'  },
 ];
 
 //export const App.routingRoutes = RouterModule.forChild(routes);
 
 @NgModule({
   imports:RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
-
 })
 
 export class AppRoutingModule {}
