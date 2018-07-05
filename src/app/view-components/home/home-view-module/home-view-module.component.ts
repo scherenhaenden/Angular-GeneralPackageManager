8da7d4bc2bc78f727733b80d3eab4f0305d2e253
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-05 13:16:51 +0200

    Home view Module created

---
+++ src/app/view-components/home/home-view-module/home-view-module.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-view-module',
+  templateUrl: './home-view-module.component.html',
+  styleUrls: ['./home-view-module.component.css']
+})
+export class HomeViewModuleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/view-components/home/home-view-module/home-view-module.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeViewModuleComponent } from './home-view-module.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HomeViewModuleComponent]
+})
+export class HomeViewModuleModule { }
