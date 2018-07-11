ec4578abf313bdbb6491bb48f97382bfa175d414
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-11 10:29:38 +0200

    Adding Module for Packages

---
+++ src/app/view-components/child-view-components/packages-views/packages-views.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-packages-views',
+  templateUrl: './packages-views.component.html',
+  styleUrls: ['./packages-views.component.css']
+})
+export class PackagesViewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
