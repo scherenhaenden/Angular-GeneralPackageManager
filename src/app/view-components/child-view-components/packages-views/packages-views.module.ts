c99b5b5a9366043ee121f7123b991809247eafc1
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-11 10:30:03 +0200

    Module of Packages view

---
+++ src/app/view-components/child-view-components/packages-views/packages-views.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PackagesViewsComponent } from './packages-views.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PackagesViewsComponent]
+})
+export class PackagesViewsModule { }
