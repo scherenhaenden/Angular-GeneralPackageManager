bf4733feac8719c2b20ad52d44314519528a091c
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-12 15:00:35 +0200

    new Method to find the packages by autocomplete

--- src/app/tools/services/nuget/nuget.service.ts
+++ src/app/tools/services/nuget/nuget.service.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { NugetRoutes } from '../../packages/nuget/services.routes/nuget.routes';
 import { ActivatedRoute } from '@angular/router';
+import { SearchAutoCompleteResponseModel } from '../../../models/packages/services/SearchAutoCompleteResponse.model';
 
 @Injectable(/*{
  // providedIn: 'root',
@@ -22,8 +23,13 @@ export class NugetService {
         return this.http.get(this.nugetRoutes.IndexURL);
     }
 
-    public findPackageStartingWith(): any{
-        return this.http.get(this.nugetRoutes.IndexURL);
+    public findPackageStartingWith(nameOfPackage: string): Observable<object>{
+
+        // GET {@id}?q={QUERY}&skip={SKIP}&take={TAKE}&prerelease={PRERELEASE}&semVerLevel={SEMVERLEVEL}
+        // GET https://api-v2v3search-0.nuget.org/autocomplete?q=storage&prerelease=true
+
+        let urlWithQuery = this.nugetRoutes.SearchAutocompleteService + `?q=${nameOfPackage}`;         
+        return this.http.get(urlWithQuery);
     }
 
 
