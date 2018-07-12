50e5683957c03e998b340243b87ebd740baacdc6
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-12 13:21:37 +0200

    Routes for Nuget updated

---
+++ src/app/tools/packages/nuget/services.routes/nuget.routes.ts
@@ -0,0 +1,16 @@
+export class NugetRoutes {
+    public IndexURL: string;
+    public SearchAutocompleteService: string;
+
+    constructor(){
+        
+    }
+
+    private fillRoutes(){
+        this.IndexURL = 'https://api.nuget.org/v3/index.json';
+        this.SearchAutocompleteService = 'https://api-v2v3search-0.nuget.org/autocomplete';
+
+    }
+
+
+}
