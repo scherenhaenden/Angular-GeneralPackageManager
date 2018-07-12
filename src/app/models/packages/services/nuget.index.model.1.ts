57359454d82f26306970d27c6b35d7315c393593
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-12 12:02:50 +0200

    Test. it has to be deleted before merging

---
+++ src/app/models/packages/services/nuget.index.model.1.ts
@@ -0,0 +1,34 @@
+export interface Weather {
+    @context: @context;
+    totalHits: number;
+    lastReopen: string;
+    index: string;
+    data?: (DataEntity)[] | null;
+  }
+ /* export interface @context {
+    @vocab: string;
+    @base: string;
+  }
+  export interface DataEntity {
+    @id: string;
+    @type: string;
+    registration: string;
+    id: string;
+    version: string;
+    description: string;
+    summary: string;
+    title: string;
+    licenseUrl: string;
+    projectUrl?: string | null;
+    tags?: (string | null)[] | null;
+    authors?: (string)[] | null;
+    totalDownloads: number;
+    verified: boolean;
+    versions?: (VersionsEntity)[] | null;
+  }
+  export interface VersionsEntity {
+    version: string;
+    downloads: number;
+    @id: string;
+  }*/
+  
