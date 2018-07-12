cb309f8df9efc6762bf86cb9336fef445b6e6738
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-12 13:22:20 +0200

    Nuget Service created and updated

---
+++ src/app/tools/services/nuget/nuget.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { NugetRoutes } from '../../packages/nuget/services.routes/nuget.routes';
+import { ActivatedRoute } from '@angular/router';
+
+@Injectable(/*{
+ // providedIn: 'root',
+}*/)
+
+export class NugetService {
+
+    private indexEndpoint: string; 
+    private nugetRoutes = new NugetRoutes();
+
+    constructor(private http: HttpClient,
+        private route: ActivatedRoute
+
+    ) { }
+
+    public getIndexService(): any{
+
+        //alert('e')
+
+        return this.http.get(this.nugetRoutes.IndexURL);
+
+
+    }
+
+
+}
