d67310e5251146d479466af42c1cf3705104e9cc
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-12 12:03:25 +0200

    another test that hast to be deteled, renamed, changed or fixed before merging

---
+++ src/app/models/packages/services/test.ts
@@ -0,0 +1,16 @@
+import INugetIndexModel = require('./nuget.index.model');
+//import * as emitter from './nuget.index.model.ts'
+
+
+export class SwapManager  {
+    emitter = new INugetIndexModel.Context();
+
+    public searchPackage(): void {
+
+//        this.emitter.
+
+       
+        
+      }
+  
+}
