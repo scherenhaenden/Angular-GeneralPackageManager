f803c77e5f1cfa71806eedb6a86287c3eca1c159
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-13 13:23:46 +0200

    making Packageservice as pipe line

--- src/app/tools/services/generic/package.service.ts
+++ src/app/tools/services/generic/package.service.ts
@@ -1,11 +1,12 @@
-
+import { async } from '@angular/core/testing';
 import { SearchAutoCompleteResponseModel } from "../../../models/packages/services/SearchAutoCompleteResponse.model";
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { NugetRoutes } from '../../packages/nuget/services.routes/nuget.routes';
 import { ActivatedRoute } from '@angular/router';
 import { NugetService } from "src/app/tools/services/nuget/nuget.service";
+import { GenericPackage } from "../../../models/packages/generic.package";
+import { filter, map, take, tap } from 'rxjs/operators';
 
 
 @Injectable(/*{
@@ -22,14 +23,76 @@ export class PackageService {
 
     public searchAutoCompleteResponseModel = new SearchAutoCompleteResponseModel();
 
-    public getPackages(packageName:string): any {
+    public getPackages(packageName:string):any {
+        
         /*this.nugetService.findPackageStartingWith(packageName)
-            .subscribe(data => {      
-              this.searchAutoCompleteResponseModel= <SearchAutoCompleteResponseModel>data;                    
+            .subscribe(data => {    
+              
+              this.searchAutoCompleteResponseModel= data;                    
+              this.parseObects();
+              return data;
+
         });*/
-        let response = this.nugetService.findPackageStartingWith(packageName);
-        return response;
+
+        let obs= this.nugetService.findPackageStartingWith(packageName);
+
+        
+       
+
+        obs.subscribe(data => {    
+              
+            this.searchAutoCompleteResponseModel= data;
+            
+        
+
+            //this.parseObects();
+            
+            return data;
+
+      });
+
+
+
+        /*let response = this.nugetService.findPackageStartingWith(packageName);
+        return response;*/
     }
+
+    public async  getPackages2(packageName:string):Promise<GenericPackage[]> {
+        let be = await this.nugetService.findPackageStartingWith2(packageName);
+
+        let be2 = this.parseObects(be);
+        
+        
+
+        let b3 = new Promise<GenericPackage[]>(resolve =>{be2});
+
+        
+
+
+
+        //return this.nugetService.findPackageStartingWith2(packageName);
+        return b3;
+            
+        
+        /*let response = this.nugetService.findPackageStartingWith(packageName);
+        return response;*/
+    }
+
+    
+
+    private parseObects(searchAutoCompleteResponseModel: SearchAutoCompleteResponseModel): GenericPackage[]{
+        let genericPackages = new Array<GenericPackage> ();
+        let genericPackage = new GenericPackage() ;
+        for (let entry of searchAutoCompleteResponseModel.data) {
+          genericPackage = new GenericPackage() ;
+          genericPackage.Name = entry;
+          genericPackages.push(genericPackage);
+        }
+        return genericPackages;
+      }
+
+
+
 
     
 
