1594c17cee0a50393dde9ac80642f7b933572729
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-16 12:35:05 +0200

    this might gonna be deleted

---
+++ src/app/models/packages/services/nuget/test.ts
@@ -0,0 +1,16 @@
+//import * = require('./nuget.index.model');
+//import * as emitter from './nuget.index.model.ts'
+
+
+export class SwapManager  {
+    
+
+    public searchPackage(): void {
+
+//        this.emitter.
+
+       
+        
+      }
+  
+}
