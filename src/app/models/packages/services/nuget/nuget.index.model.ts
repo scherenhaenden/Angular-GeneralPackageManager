dbac1188bef89ccc12af0def66d4fb246d716831
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-16 12:35:43 +0200

    nuget model moved to its own nuget directory

---
+++ src/app/models/packages/services/nuget/nuget.index.model.ts
@@ -0,0 +1,41 @@
+declare module NugetIndexModel {
+
+    export class Context {
+        '@vocab': string;
+        '@base': string;
+    }
+
+    export class Version {
+        version: string;
+        downloads: number;
+        '@id': string;
+    }
+
+    export class Datum {
+        '@id': string;
+        '@type': string;
+        registration: string;
+        id: string;
+        version: string;
+        description: string;
+        summary: string;
+        title: string;
+        licenseUrl: string;
+        projectUrl: string;
+        tags: string[];
+        authors: string[];
+        totalDownloads: number;
+        verified: boolean;
+        versions: Version[];
+    }
+
+    export class RootObject  {
+        public '@context': Context;
+        totalHits: number;
+        lastReopen: Date;
+        index: string;
+        data: Datum[];
+    }
+
+}export default NugetIndexModel;
+
