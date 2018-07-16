14612dd5d77f232658e4fe43e55c8a6c9d3e8f56
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-16 12:33:23 +0200

    Interface for Generic Package Parser created

--- src/app/tools/parsers/igenerick.package.parser.ts
+++ src/app/tools/parsers/igenerick.package.parser.ts
@@ -1,6 +1,6 @@
-import { SearchAutoCompleteResponseModel } from "../../models/packages/services/SearchAutoCompleteResponse.model";
+import { SearchAutoCompleteResponseModel } from "../../models/packages/services/nuget/SearchAutoCompleteResponse.model";
 import { GenericPackage } from "../../models/packages/generic.package";
-import { Injectable } from "node_modules/@angular/core";
+
 
 export interface IgenerickPackageParser {
     parseSeachPackagesNugetToGenericPackage(searchAutoCompleteResponseModel: SearchAutoCompleteResponseModel): GenericPackage[]
