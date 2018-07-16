ffe7b05b2de8385844f244e1a30cd24ada01f160
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-16 12:33:05 +0200

    getPackages updated, now it works for NPM too

--- src/app/tools/services/generic/package.service.ts
+++ src/app/tools/services/generic/package.service.ts
@@ -1,14 +1,16 @@
 import { async } from '@angular/core/testing';
-import { SearchAutoCompleteResponseModel } from "../../../models/packages/services/SearchAutoCompleteResponse.model";
+import { SearchAutoCompleteResponseModel } from "../../../models/packages/services/nuget/SearchAutoCompleteResponse.model";
 import { Injectable } from '@angular/core';
 import { Observable, of, observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
-import { NugetService } from "src/app/tools/services/nuget/nuget.service";
+import { NugetService } from '../nuget/nuget.service';
 import { GenericPackage } from "../../../models/packages/generic.package";
 import { filter, map, take, tap } from 'rxjs/operators';
-import { promise } from '../../../../../node_modules/protractor';
+import { promise } from 'protractor';
 import { GenerickPackageParser } from '../../parsers/generick.package.parser';
+import { NPMService } from '../npm/npm.service';
+import { NugetSearchResponseModel } from '../../../models/packages/services/npm/nuget.search.response.model';
 
 
 @Injectable(/*{
@@ -18,6 +20,7 @@ import { GenerickPackageParser } from '../../parsers/generick.package.parser';
 export class PackageService {    
 
     constructor(private nugetService: NugetService
+                ,private nPMService: NPMService
                ,private http: HttpClient
                 ,private route: ActivatedRoute
                 ,public generickPackageParser: GenerickPackageParser
@@ -26,18 +29,46 @@ export class PackageService {
 
     public searchAutoCompleteResponseModel = new SearchAutoCompleteResponseModel();
 
-    public async  getPackages(packageName:string):Promise<GenericPackage[]> {
-        let packagesToGetResolved =  this.nugetService.findPackageStartingWith2(packageName).
+    public async  getPackages(packageName:string, selectedPackageSystem:string):Promise<GenericPackage[]> {
+        
+        
+        if(selectedPackageSystem === 'Nuget'){
+                let packagesToGetResolved =  this.nugetService.findPackageStartingWithPromise(packageName).
+            then(
+                value=>{
+                    return this.parseObects(selectedPackageSystem, value);
+                }
+            );
+            return packagesToGetResolved;  
+        }
+        else if (selectedPackageSystem === 'NPM'){
+            let packagesToGetResolved =  this.nPMService.findPackageStartingWithPromise(packageName).
         then(
             value=>{
-                return this.parseObects(value);
+                return this.parseObects(selectedPackageSystem, value);
             }
         );
-        return packagesToGetResolved;       
+        return packagesToGetResolved;  
     }
 
-    private parseObects(searchAutoCompleteResponseModel: SearchAutoCompleteResponseModel): GenericPackage[]{
-        return this.generickPackageParser.parseSeachPackagesNugetToGenericPackage(searchAutoCompleteResponseModel);       
+
+
+
+             
+    }
+
+    private parseObects(selectedPackageSystem:string, responseModel: any): GenericPackage[]{
+        
+        if(selectedPackageSystem === 'Nuget'){
+            return this.generickPackageParser.parseSeachPackagesNugetToGenericPackage(<SearchAutoCompleteResponseModel><any>responseModel); ;  
+        }
+        else if(selectedPackageSystem === 'NPM'){
+            console.log(responseModel);
+            return this.generickPackageParser.parseSeachPackagesNPMToGenericPackage(<NugetSearchResponseModel><any>responseModel); ;  
+        }
+
+
+        //return this.generickPackageParser.parseSeachPackagesNugetToGenericPackage(searchAutoCompleteResponseModel);       
     }
 
 
