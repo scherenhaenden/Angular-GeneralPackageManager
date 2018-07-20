60ac6f3c6225496b31b2c44fef9a30c9b1755b1c
scherenhaenden@gmail.com <scherenhaenden@gmail.com>
2018-07-20 14:53:17 +0200

    Lua service refactores now it is going to run as Electron App

--- src/app/tools/services/lua.rocks/lua.rocks.service.ts
+++ src/app/tools/services/lua.rocks/lua.rocks.service.ts
@@ -3,6 +3,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { LuaRocksRoutes } from '../../packages/nuget/services.routes/luarocks.routes';
 import { LuaRocksManifestRootResponse } from '../../../models/packages/services/lua.rocks/lua.rocks.manifest.root.response';
+import { MockingLoadPackaged } from '../../../Mock/mocking.load.packaged';
+import { MockingWirdLuarocksData } from '../../../Mock/moking.lua.rocks.weird.data';
+
+
 
 @Injectable(/*{
  // providedIn: 'root',
@@ -22,23 +26,14 @@ export class LuaRockservice {
         return this.http.get(this.luaRocksRoutes.Manifest);
     }
 
-    public findPackageStartingWithPromise(nameOfPackage: string): Promise<LuaRocksManifestRootResponse>{
-
-        // GET {@id}?q={QUERY}&skip={SKIP}&take={TAKE}&prerelease={PRERELEASE}&semVerLevel={SEMVERLEVEL}
-        // GET https://api-v2v3search-0.nuget.org/autocomplete?q=storage&prerelease=true
-
-
-        //let urlWithQuery = this.nPMRoutes.SearchAutocompleteService + `/${nameOfPackage}`;         
-
-        let headers = new HttpHeaders().
-        set('Access-Control-Allow-Origin','*').        
-        set('Access-Control-Allow-Headers','Content-Type').
-        set('Content-Type','text/json').
-        set('Access-Control-Allow-Methods','GET,POST');
+    public findPackageStartingWithPromise(nameOfPackage: string): Promise<LuaRocksManifestRootResponse>{        
 
         let urlWithQuery = this.luaRocksRoutes.Manifest;         
+
         
-        let response = <Promise<LuaRocksManifestRootResponse>>this.http.get(urlWithQuery,{headers:headers}).toPromise();        
+        
+        let response = <Promise<LuaRocksManifestRootResponse>>this.http.get(new MockingWirdLuarocksData().getdata()).toPromise();        
+               
         return response;
     }
 
